3ba6511dc9ceb0a50c8be0bedc6003faf0eddaf8
Xavier Perez <xap5xap@users.noreply.github.com>
2018-03-29 08:00:36 -0500

    Add fecth api code

---
+++ App/Services/Api.js
@@ -0,0 +1,19 @@
+import apisauce from 'apisauce'
+
+const create = (baseURL = 'http://www.thecocktaildb.com/api/json/v1/1') => {
+
+    const api = apisauce.create({
+        baseURL,
+        timeout: 15000
+    })
+
+    const getGlassCocktails = () => api.get('/filter.php', { g: 'Cocktail_glass' })
+
+    return {
+        getGlassCocktails,
+    }
+}
+
+export default {
+    create
+}
